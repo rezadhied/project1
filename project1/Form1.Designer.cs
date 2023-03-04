01b173bffbcacf0e22b501b9f904c8a9b13ed6e7
rezadhied <rezadhied@student.telkomuniversity.ac.id>
2023-03-04 21:09:03 +0700

    tambahan fitur GUI

--- project1/Form1.Designer.cs
+++ project1/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(487, 135);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(163, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit/Kirim";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(109, 135);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(347, 49);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(106, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(544, 49);
+            this.label1.TabIndex = 2;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(768, 449);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- project1/Form1.cs
+++ project1/Form1.cs
@@ -15,6 +15,29 @@ namespace project1
         public Form1()
         {
             InitializeComponent();
+            this.BackColor = Color.LightGray;
+            label1.BackColor = Color.White;
+            textBox1.Font = new Font("Times New Roman", 12);
+            button1.Font = new Font("Times New Roman", 12);
+            label1.Font = new Font("Times New Roman", 14);
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            AcceptButton = button1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Halo "+textBox1.Text;
         }
     }
 }
